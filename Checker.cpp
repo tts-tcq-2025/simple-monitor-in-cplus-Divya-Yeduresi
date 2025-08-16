42d98d7f4483f27137125a788a07b5984ee43e1a
Divya-Yeduresi <Yeduresi.VenkataNagaDivyasree@bosch.com>
2025-08-16 10:26:21 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+#include <stdio.h>
 #include <assert.h>
 #include <iostream>
 using namespace std;
@@ -14,6 +16,49 @@ bool batteryIsOk(float temperature, float soc, float chargeRate) {
     return false;
   }
   return true;
+
+bool temperatureIsOK(float temperature)
+{
+	if(temperature < 0 || temperature > 45) 
+	{
+		printf( "Temperature out of range!\n");
+		return false;
+	}
+	return true;
+}
+
+bool socIsOK(float soc)
+{
+	if(soc < 20 || soc > 80) 
+	{
+		printf( "State of Charge out of range!\n");
+		return false;
+	}
+	return true;
+}
+bool socIsOK(float soc)
+{
+	if(soc < 20 || soc > 80) 
+	{
+		printf( "State of Charge out of range!\n");
+		return false;
+	}
+	return true;
+}
+
+bool chargeRateIsOK(float chargeRate)
+{
+	if(chargeRate > 0.8)
+	{
+		printf( "Charge Rate out of range!\n");
+		return false;
+	}
+	return true;
+}
+
+bool batteryIsOk(float temperature, float soc, float chargeRate) 
+{
+	return ((temperatureIsOK(temperature)) && (socIsOK(soc)) && (chargeRateIsOK(chargeRate)));
 }
 
 int main() {
